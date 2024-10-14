1d41648a274c406103258a0efda8ce738778fb4c
trollstorex <59131675+TrollStoreX@users.noreply.github.com>
2024-10-14 22:56:57 +0800

    Create com.nan.crc.js

---
+++ Library/Preferences/com.nan.crc.js
@@ -0,0 +1,28 @@
+{
+  "bark": {
+    "is_archive": 0,
+    "Hello_World": 0,
+    "level": "active",
+    "title": "来自 crc - repo_name 已变更",
+    "group": "com.nan.crc",
+    "body": {
+      "add": "下列软件包已新增：",
+      "del": "下列软件包已移除：",
+      "up": "下列软件包已更新："
+    },
+    "sound": "healthnotification"
+  },
+  "enable": 1,
+  "use_name": 0,
+  "filter_architecture": "all",
+  "source": [
+    {
+      "enable": 1,
+      "url": "https://shalamand3r.github.io/"
+    },
+    {
+      "enable": 1,
+      "url": "https://poomsmart.github.io/repo/"
+    }
+  ]
+}
